f4e114a54339643fa3e898cfef4cc78f1ecab1e8
Kevin Langat <langatkaykay@gmail.com>
2025-07-27 11:27:22 +0300

    Modified bookshelf app

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,7 +1,10 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
     publication_year = models.IntegerField()
+    
+    # Add this to track which user added the book
+    added_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
