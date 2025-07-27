5e13451d6bc062196baf495d05798cb47799899b
Kevin Langat <langatkaykay@gmail.com>
2025-07-27 20:12:05 +0300

    Modified views again

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.decorators import permission_required
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Book
-from .forms import BookForm  # ✅ Import the ModelForm
+from .forms import ExampleForm  # ✅ Import the ModelForm
 
 
 @permission_required('bookshelf.can_view', raise_exception=True)
@@ -12,12 +12,12 @@ def book_list(request):
 @permission_required('bookshelf.can_create', raise_exception=True)
 def book_create(request):
     if request.method == 'POST':
-        form = BookForm(request.POST)
+        form = ExampleForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect('book_list')
     else:
-        form = BookForm()
+        form = ExampleForm()
     return render(request, 'bookshelf/book_form.html', {'form': form})
 
 
@@ -25,12 +25,12 @@ def book_create(request):
 def book_edit(request, pk):
     book = get_object_or_404(Book, pk=pk)
     if request.method == 'POST':
-        form = BookForm(request.POST, instance=book)
+        form = ExampleForm(request.POST, instance=book)
         if form.is_valid():
             form.save()
             return redirect('book_list')
     else:
-        form = BookForm(instance=book)
+        form = ExampleForm(instance=book)
     return render(request, 'bookshelf/book_form.html', {'form': form})
 
 @permission_required('bookshelf.can_delete', raise_exception=True)
