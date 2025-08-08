b26c2484907108476dc43f3cdd193986035ea0b9
Kevin Langat <langatkaykay@gmail.com>
2025-08-08 16:56:44 +0300

    Added ViewSets

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,6 +1,15 @@
-from django.urls import path
-from .views import BookList
+from django.urls import include, path
+from rest_framework.authtoken.views import obtain_auth_token
+from .views import BookList, BookViewSet
+from rest_framework.routers import DefaultRouter
+
+# Create a router and register our viewset with it.
+router = DefaultRouter()
+router.register(r'books_all', BookViewSet, basename='book_all')
 
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),  # Maps to the BookList view
+    path('api/token-auth/', obtain_auth_token, name='api_token_auth'),
+    # Include the router URLs
+    path('', include(router.urls)),
 ]
