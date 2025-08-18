506bca3d4ee18665f3f028620c98fea410b50557
Kevin Langat <langatkaykay@gmail.com>
2025-08-18 15:44:20 +0300

    registered posts.urls in main urls

--- social_media_api/social_media_api/urls.py
+++ social_media_api/social_media_api/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('accounts.urls')),  # Include the accounts app URLs
+    path('accounts/', include('accounts.urls')), 
+    path('posts/', include('posts.urls')),
 ]
