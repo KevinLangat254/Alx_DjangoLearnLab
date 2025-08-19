284002881bf9ec061589d20b8bd979a29023c556
Kevin Langat <langatkaykay@gmail.com>
2025-08-19 15:50:09 +0300

    Modified posts/urls

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -5,7 +5,7 @@ from .views import PostViewSet, CommentViewSet, FeedViewSet
 router = DefaultRouter()
 router.register(r'posts', PostViewSet, basename='post')
 router.register(r'comments', CommentViewSet, basename='comment')
-router.register(r'feed', FeedViewSet, basename='feed')
+router.register(r'feed/', FeedViewSet, basename='feed')
 
 urlpatterns = [
     path("", include(router.urls)),
