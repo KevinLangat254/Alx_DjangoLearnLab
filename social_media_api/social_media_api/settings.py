41baf128cd4c8f204d38e32c6b5e96865cad9cb7
Kevin Langat <langatkaykay@gmail.com>
2025-08-21 16:16:29 +0300

    Added the static configuration

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -21,6 +21,18 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Static files (CSS, JavaScript, Images)
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # 👈 required for production
+
+# Optional (for local development convenience)
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+# Media files (user uploads)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
